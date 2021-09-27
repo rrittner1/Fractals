2b35a20ea1f4becfb063e04b1f0c4e9c24e14499
rrittner1 <rrittner@purdue.edu>
2021-09-26 23:56:40 -0400

    commit 2

---
+++ src/ContentPanel.java
@@ -0,0 +1,68 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ContentPanel extends JPanel {
+    private JFrame container;
+    private static final int width = 600;
+    public ContentPanel(JFrame c) {
+        container = c;
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        double[] bounds = ((MandelBrotSet)container).getImageBounds();
+        int depth = ((MandelBrotSet)container).getDepth();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < width; j++) {
+                //System.out.println("i: " + i + "j: " + j);
+                double x = i;
+                double y = width - 1 - j;
+                /*if (i == 201 && j == 370) {
+                    System.out.println("you got this far");
+                }*/
+                Complex c = new Complex(bounds[1] * x / width + bounds[0], bounds[3] * y / width + bounds[2]);
+                int num = iterations(c);
+                int red;
+                int blue;
+                int green;
+                int d6 = depth/6;
+                int mod = num % (3*d6);
+                if (mod < d6) {
+                    red = mod;
+                    blue = 0;
+                    green = d6 - mod;
+                } else if (mod >= d6 && mod < 2*d6) {
+                    red = 2*d6 - mod;
+                    blue = mod - d6;
+                    green = 0;
+                } else {
+                    red = 0;
+                    blue = mod - 2*d6;
+                    green = 3*d6 - mod;
+                }
+                red *= 1530/depth;
+                green *= 1530/depth;
+                blue *= 1530/depth;
+                double ratio = 1.0*num/depth;
+                red += (255 - red)*ratio;
+                green += (255 - green)*ratio;
+                blue += (255 - blue)*ratio;
+                red = 255 - red;
+                green = 255 - green;
+                blue = 255 - blue;
+                g.setColor(new Color(red, blue, green));
+                g.fillRect((int)Math.round(x), (int)Math.round(y), 1, 1);
+            }
+        }
+
+
+    }
+    public int iterations(Complex c) {
+        int i = -1;
+        Complex x = new Complex(0,0);
+        while (x.mod() <= 2 && i < ((MandelBrotSet)container).getDepth()) {
+            x = c.plus(x.times(x));
+            i++;
+        }
+        return i;
+    }
+}
--- src/MandelBrotSet.java
+++ src/MandelBrotSet.java
@@ -2,17 +2,51 @@ import java.awt.*;
 import javax.swing.*;
 
 public class MandelBrotSet extends JFrame {
-    private JPanel panel;
+    private ContentPanel panel;
+    private int depth;
     public MandelBrotSet(String title) {
         super(title);
-        setSize(800, 800);
+        setSize(600, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        panel = new JPanel();
+        panel = new ContentPanel(this);
         panel.setLayout(null);
         this.getContentPane().add(panel);
+        depth = 60;
     }
-    public static void main(String args[]) {
+    public static void main(String args[]) throws InterruptedException {
+        MandelBrotSet m = new MandelBrotSet("Mandelbrot set");
+        m.setDepth(60);
+        m.setVisible(true);
+       // m.animate();
 
     }
+    public static double[] getImageBounds() {//values for picture of whole fractal shown
+        double[] out = new double[4];
+        double centerX = -0.5;//-.5
+        double centerY = 0;//0
+        double range = 3;//3
 
+        out[0] = centerX - range/2.0;
+        out[1] = range;
+        out[2] = -centerY - range/2.0;
+        out[3] = range;
+        System.out.println(out[0]);
+        System.out.println(out[1]);
+        return out;
+    }
+    public int getDepth() {
+        return depth;
+    }
+    public void setDepth(int d) {
+        depth = d;
+    }
+    public void animate() throws InterruptedException {
+        depth = 6;
+        while (depth < 1000) {
+            System.out.println("d = " + depth);
+            Thread.sleep(1);
+            depth += 1;
+            this.repaint();
+        }
+    }
 }
